17e0783d9660fb038dd5385d3246e8d1a84cad2c
g3ntl3man00 <git config --global user.email YOUR_EMAIL>
2023-11-06 04:20:26 +0500

    :

--- python_sandbox_starter/loops.py
+++ python_sandbox_starter/loops.py
@@ -1,5 +1,40 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['John','Karen','Janet', 'Will']
+# Simple for loop
+
+# for person in people: 
+#  print('Current person', person)    
+# break
+# for person in people: 
+#  if person =='Karen': 
+#   break 
+#  print('Current person', person)   
+
+# continue 
+# for person in people: 
+#  if person =='Janet': 
+#   continue 
+#  print('Current person', person)   
+
+# Range
+# for i in range(len(people)):
+#   print('Current person', people[i])   
 
 
 # While loops execute a set of statements as long as a condition is true.
+
+count = 0 
+while count <= 10:
+ print('Count: ' , count) 
+
+count +=1 
+
+
+
+
+
+
+
+
+
