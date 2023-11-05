32b8cc6193f76ce6754ab4ba9b9d86ff013cd9f7
g3ntl3man00 <ibear8972@gmail.com>
2023-11-06 02:53:16 +0500

    ,

--- python_sandbox_starter/conditionals.py
+++ python_sandbox_starter/conditionals.py
@@ -1,19 +1,36 @@
 # If/ Else conditions are used to decide to do something based on something being true or false
 
-
+x=8
+y=5
 
 # Comparison Operators (==, !=, >, <, >=, <=) - Used to compare values
 
+# if simple
+# if x==y:
+#  print(f'{x} is equal to {y}')   
 
+# if/else 
+# if x>y: 
+#  print(f'{x} is greater than {y}') 
+# else:
+#   print(f'{x} is less than {y}')
 
 # Logical operators (and, or, not) - Used to combine conditional statements
 
+# and 
+# if x > 2 and x <=10: 
+#  print (f'{x} is less than 10 and greater than 0 ')
 
-
+# if x > 2 or x <=10: 
+#  print (f'{x} is less than 10 and greater than 0 ')
+# if not(x==y): 
+#  print (f'{x} is not equal to {y}')
 
 # Membership Operators (in, not in) - Membership operators are used to test if a sequence is presented in an object
 
+number = [1,2,3,4,5]
 
-
+if x not in number:
+ print(x in number)   
 
 # Identity Operators (is, is not) - Compare the objects, not if they are equal, but if they are actually the same object, with the same memory location:
