82d410eed816557e4163a5cd71ffed6d130c69e6
g3ntl3man00 <ibear8972@gmail.com>
2023-11-06 00:33:51 +0500

    ,

--- python_sandbox_starter/tuples_sets.py
+++ python_sandbox_starter/tuples_sets.py
@@ -1,6 +1,9 @@
 # A Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
- fruit_tuple = ('Mangi', 'bannan', 'Crean','fick', 'dick')
+fruit_tuple = ('Mangi', 'bannan', 'Crean','fick', 'dick')
 
-print(fruit_tuple[1]) 
 # A Set is a collection which is unordered and unindexed. No duplicate members.
+fruit_set = {'apple','orange', 'mango'}
 
+
+
+print('apple 'in fruit_set) 
