dc0843ca9e5453517da8345d5e13ce0fbf9b6a08
g3ntl3man00 <ibear8972@gmail.com>
2023-11-05 06:15:08 +0500

    m

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -3,9 +3,49 @@
 #age = 40
 #print("My name is "+ str(name) +" and I am "+ str(age) +" years old!")
 
+
+
+
+
 # String Formatting
+# name = 'arman here was gud' 
+# age = 23
+
+# Concatenate
+# print('My name is ' + name + 'and My age is ' +str (age)) 
+
+
+#Arguments by position  
+# print('{}, {}, {}'.format('a','b','c'))
+# print('{1}, {2}, {0}'.format('a','b','c'))
+  
+# Argument by name 
+
+
+# F-Strings (only in 3.6 and above ) 
+# print(f'my name is {name} and I am {age}')
+
+ 
+name = 'arman here was gud' 
+age = 23 
 
 # String Methods
-name = input("Whats your name? ")
-age = int(input("Whats your age? "))
-print("Hello "+ name.upper() +" how are you ?")
+# print(name.capitalize())
+
+
+# Make everything Upper case
+print(name.upper())
+
+# Make all lower case
+print(name.lower()) 
+
+
+# Get length of string 
+print(len(name))
+
+# name = input("Whats your name? ")
+# age = int(input("Whats your age? "))
+# print("Hello "+ name.upper() +" how are you ?")
+
+
+
