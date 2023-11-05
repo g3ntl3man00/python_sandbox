25d5158fd29b89d58f7ff8484215b20e194a9b2f
g3ntl3man00 <ibear8972@gmail.com>
2023-11-06 01:08:02 +0500

    ,

--- python_sandbox_starter/dictionaries.py
+++ python_sandbox_starter/dictionaries.py
@@ -1,2 +1,24 @@
 # A Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
 # Read more about dictionaries at https://docs.python.org/3/tutorial/datastructures.html#dictionaries
+
+# Simple dictionary  
+person= {
+    'first_name':'John ',
+    'last_name':'Doe',
+    'age' : 19,
+} 
+
+# you can use the constructor too  
+
+# if you wanna access an item you could do sum like dis 
+
+# person.get('age')
+
+
+# Add values 
+# person['phone'] = '555-555-5555' 
+
+
+print(person)
+
+
