6c730abc848cbd230ef0f3b7ec236468468e2286
g3ntl3man00 <git config --global user.email YOUR_EMAIL>
2023-11-06 05:45:29 +0500

    m

--- python_sandbox_starter/modules.py
+++ python_sandbox_starter/modules.py
@@ -1,1 +1,26 @@
 # A module is basically a file containing a set of functions to include in your application. There are core python modules, modules you can install using the pip package manager (including Django) as well as custom modules
+
+
+# core modules 
+
+from time import time
+timestamp = time()
+
+print (timestamp) 
+# pip module
+import camelcase
+# Custom modules 
+
+from validator import validate_email
+
+camel = camelcase.CamelCase()
+text= 'hello there everyone'
+print(camel.hump(text))
+
+email = 'test@test.com'
+if validate_email(email):
+ print('email is valid') 
+else:
+ print ('email is not valid')    
+
+
